db8204d878515ba2a3dc4c0a0915e1b84d22c9ec
Eric Anholt <eric@anholt.net>
2021-02-25 14:31:23 -0800

    Use ccache for the NDK build.
    I got tired of ridiculous build times when tweaking build options.

--- ndk-build.sh
+++ ndk-build.sh
@@ -1,3 +1,4 @@
+export NDK_CCACHE=`which ccache`
 export NDK_PROJECT_PATH=`pwd`
 ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-$HOME/android-ndk-r13b}
 $ANDROID_NDK_ROOT/build/ndk-build NDK_APPLICATION_MK=./Application.mk "$@"
